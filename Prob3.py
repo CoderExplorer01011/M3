8f6b290c3625a7ab9561d12b3d240831bc6e6f27
David Ramirez <daver3754@gmail.com>
2024-10-08 14:51:30 -0500

    Add files via upload

---
+++ Prob3.py
@@ -0,0 +1,7 @@
+#David Ramirez
+#10/08/2024
+pi = int(3.14)
+radius = float(input("Insert your radius: "))
+cal = pi*radius*radius
+print("Here is your calulation", cal)
+#This program grabs the users radius and muntiples sqaured with pi
