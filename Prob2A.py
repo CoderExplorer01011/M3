dd48424de5676b43524f8428e43fd26c7ff78ddf
David Ramirez <daver3754@gmail.com>
2024-10-08 14:32:41 -0500

    Add files via upload

---
+++ Prob2A.py
@@ -0,0 +1,13 @@
+#David Ramirez
+#10/8/2024
+username = input("insert name: ") 
+name1 = "David Ramirez"
+name2 = "Benjamin"
+
+if username == name1:
+    print("Hello",name1)
+elif username == name2:
+    print("Hello",name2) 
+else:
+    print("Unauthorized User!")
+#This programs decides whether a user is authorized or unauthorized
