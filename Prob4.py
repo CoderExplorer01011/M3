060cc5d6355e32d51de930ab8eb55aca17c13bda
David Ramirez <daver3754@gmail.com>
2024-10-08 15:07:13 -0500

    Add files via upload

---
+++ Prob4.py
@@ -0,0 +1,7 @@
+#David Ramirez 
+#10/8/2024
+Mileage = int(input("Insert number of miles driven: "))
+Gallons = int(input("Insert number of gallon space: "))
+Cal = Mileage/Gallons
+print("Here is your MPG! ",Cal)
+#Calculates the MPG by dividing Miles driven by gallons hold by the vehicle
